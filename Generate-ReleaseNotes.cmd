8693adb05f60da320e3a9de9efb55c6aa382b1a2
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-05-06 18:13:54 +0200

    001

--- Generate-ReleaseNotes.cmd
+++ Generate-ReleaseNotes.cmd
@@ -1,7 +1,9 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
+rem --skip-empty-releases
+
 SET version=0.0.1
 
-GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc --version %version% --token %GH_TOKEN%
+GitHubReleaseNotes --output ReleaseNotes.md --exclude-labels question invalid doc --version %version% --token %GH_TOKEN%
 
-GitHubReleaseNotes --output PackageReleaseNotes.txt --skip-empty-releases --exclude-labels question invalid doc --template PackageReleaseNotes.template --version %version% --token %GH_TOKEN%
+GitHubReleaseNotes --output PackageReleaseNotes.txt --exclude-labels question invalid doc --template PackageReleaseNotes.template --version %version% --token %GH_TOKEN%
