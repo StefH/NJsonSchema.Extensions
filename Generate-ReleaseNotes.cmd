0bba2f955392cee6bcb2d6f6728f331098508a55
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-05-13 08:27:14 +0200

    0.1.0

--- Generate-ReleaseNotes.cmd
+++ Generate-ReleaseNotes.cmd
@@ -2,7 +2,7 @@ rem https://github.com/StefH/GitHubReleaseNotes
 
 rem --skip-empty-releases
 
-SET version=0.0.1
+SET version=0.1.0
 
 GitHubReleaseNotes --output ReleaseNotes.md --exclude-labels question invalid doc --version %version% --token %GH_TOKEN%
 
--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,3 @@
-# 0.0.1 (06 May 2022)
+# 0.1.0 (13 May 2022)
+- [#1](https://github.com/StefH/NJsonSchema.Extensions/pull/1) - Handle JArray and JObject in object.ToJsonSchema  [enhancement] contributed by [StefH](https://github.com/StefH)
 
